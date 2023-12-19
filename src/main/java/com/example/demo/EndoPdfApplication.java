f8477e77228293e4541cbe95ab50efba70b2a847
alexsarabiatoledo <a.sarabia02@ufromail.cl>
2023-12-19 10:43:46 -0300

    commit inicial

---
+++ src/main/java/com/example/demo/EndoPdfApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndoPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndoPdfApplication.class, args);
+	}
+
+}
